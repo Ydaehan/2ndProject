6d724f94711d7f5be8b7ae77e9fc07462254d102
Ydaehan <daehan821@naver.com>
2023-03-13 15:39:29 +0900

    '23.03.13.(월)

--- edit_ok.php
+++ edit_ok.php
@@ -24,14 +24,22 @@ echo "닉네임 : ".$nickname."<br>";
 include "db.php";
 
 /* 쿼리 작성 */
-if(!$pw){
+if(!$pw && $nickname){ // 닉네임이 공백이 아니고 pw 가 비워져 있을 때
   $sql = "update manage_user set nickname = '$nickname' where user_id = '$user_id';";
-}else if(!$nickname){
+}else if(!$nickname && $pw){ // 닉네임이 비워져있고 pw 가 공백이 아닐 때
   $sql = "update manage_user set pw = '$pw' where user_id = '$user_id';";
-}else{
+}else if($pw && $nickname){
+  // 닉네임 및 pw 가 둘 다 존재 혹은 둘 다 공백 일때의 조건 부여 필요
   $sql = "update manage_user set pw = '$pw', nickname = '$nickname' where user_id = '$user_id';";
+}else {
+  // 닉네임과 pw 가 둘 다 공백일때는 아무것도 안하고 정보수정 안되게 할수 없을까?
+  echo "<script type=\"text/javascript\">
+  alert(\"닉네임과 비밀번호가 비워져있습니다. 홈으로 돌아갑니다.\");
+  location.href = \"index.php\";
+  </script>";
 }
 echo $sql;
+
 
 /* 데이터베이스에 쿼리 전송 */
 mysqli_query($db,$sql);
@@ -43,8 +51,6 @@ $nickname = mysqli_fetch_array($result);
 if($same){
   $_SESSION['user_nickname'] = $nickname["nickname"];
 }
-
-
 
 /* DB(연결) 종료 */
 mysqli_close($db);
